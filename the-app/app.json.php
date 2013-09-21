f7b341982629521257be17369d5436eb66106487
Trevor Mills <t@topquark.com>
2013-09-21 00:27:01 -0400

    removed some unnecessary files - trying to reduce the download footprint of the repo

--- the-app/app.json.php
+++ the-app/app.json.php
@@ -61,7 +61,7 @@
 	     */
 	    "css" => array(
 	    	array(
-	            "path" => ($the_app->get('sdk') == '' ? "resources/css/app.css" : "sdk{$the_app->get('sdk')}/resources/css/{$the_app->get('theme')}.css"),
+	            "path" => "sdk{$the_app->get('sdk')}/resources/css/{$the_app->get('theme')}.css"),
 	            "update" => "delta"
 	    	)
 	    ),
