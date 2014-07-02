284f7dd9aee1972d2cb2ffc300338699a0ba7a80
Trevor Mills <t@topquark.com>
2014-07-02 16:43:49 -0400

    catch localstorage exception

--- the-app/app/proxy/LocalStorage.js
+++ the-app/app/proxy/LocalStorage.js
@@ -1,6 +1,11 @@
 Ext.define('My.data.proxy.LocalStorage', {
     override: 'Ext.data.proxy.LocalStorage',
 
+	constructor: function(){
+		this.callParent( arguments );
+		this.addListener( 'exception', this.onException, this, { single: true } );
+	},
+	
     getIds: function() {
 		var ids = this.callParent( arguments );
 
@@ -12,5 +17,9 @@ Ext.define('My.data.proxy.LocalStorage', {
 			}
 		});
         return ids;
-    }
+    },
+
+	onException: function(proxy, e){
+		alert( 'This app requires more localstorage than is available.  Note to developers: in this case, it is recommended that you use another storage mechanism for your persistent data.  Try using \'sqlitestorage\' for the proxy.' );
+	}
 });
