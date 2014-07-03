3f4459e8c5b8f54ebd86cb0a1c9d560110b66ff1
Trevor Mills <t@topquark.com>
2014-07-02 21:10:07 -0400

    adding in logger plugin so console.log shows up in XCode

--- extras/cordova/ios/www/cordova_plugins.js
+++ extras/cordova/ios/www/cordova_plugins.js
@@ -6,12 +6,27 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.inappbrowser": "0.4.1-dev"
+    "org.apache.cordova.inappbrowser": "0.4.1-dev",
+	"org.apache.cordova.console": "0.2.10-dev"
 }
 // BOTTOM OF METADATA
 });
