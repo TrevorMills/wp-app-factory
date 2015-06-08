7acc5cd262de96d76e1f268af0188c70eaeb113f
Trevor Mills <t@topquark.com>
2015-06-08 17:50:12 -0400

    adding in overrides to deal with Chrome update breaking paint and overflowchanged events

--- the-app/app.js.php
+++ the-app/app.js.php
@@ -49,7 +49,32 @@ Ext.Loader.setConfig({disableCaching:false});
 
 <?php foreach ( $the_app->registered['path'] as $name ) : ?>
 Ext.Loader.setPath( '<?php echo $name; ?>', '<?php echo $the_app->registered['paths']['path'][$name]; ?>' );
-<?php endforeach; ?>	
+<?php endforeach; ?>
+
+// Chrome 43 introduced some issues with ST firing the paint event, and the overflowchange event.  the following
+// two overrides deal with that.  See discussion at http://trevorbrindle.com/chrome-43-broke-sencha/ and
+// https://www.sencha.com/forum/announcement.php?f=92&a=58
+Ext.define('Override.util.PaintMonitor', {
+    override : 'Ext.util.PaintMonitor',
+
+    constructor : function(config) {
+        return new Ext.util.paintmonitor.CssAnimation(config);
+    }
+});
+Ext.define('Override.util.SizeMonitor', {
+    override : 'Ext.util.SizeMonitor',
+
+    constructor : function(config) {
+        var namespace = Ext.util.sizemonitor;
+        if (Ext.browser.is.Firefox) {
+            return new namespace.OverflowChange(config);
+        } else if (Ext.browser.is.WebKit || Ext.browser.is.IE11) {
+            return new namespace.Scroll(config);
+        } else {
+            return new namespace.Default(config);
+        }
+    }
+});
 
 <?php if (!$the_app->is('packaging')) : ?>
 var ub = <?php $meta = $the_app->get('meta'); echo json_encode($meta['unacceptable_browser']); ?>;
